b259a0675a4c0ae14ceebd614edad8d9d9939dd9
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2019-12-28 01:26:45 +0700

    Remove async run

--- commons/patterns.py
+++ commons/patterns.py
@@ -1,13 +1,7 @@
 from abc import abstractmethod, ABC
 from typing import TypeVar, Generic
 
-T = TypeVar('T')
-
-
-class Runnable(ABC, Generic[T]):
-    @classmethod
-    async def run_async(cls, **kwargs) -> T: return cls.run(**kwargs)
-
+class Runnable(ABC):
     @classmethod
     @abstractmethod
-    def run(cls, **kwargs) -> T: ...
+    def run(cls, **kwargs) -> any: ...
