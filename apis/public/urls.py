b2a03f676099febccf559859367fb0bb9514f593
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2019-12-27 23:55:42 +0700

    Refactor URL

---
+++ apis/public/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from apis.public.views import WebhookAPI
+
+urlpatterns = [
+    path('webhook', WebhookAPI.as_view())
+]
--- webhooks/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from webhooks.views import WebhookAPI
-
-urlpatterns = [
-    path('', WebhookAPI.as_view())
-]
