e0058eb1e831cc85ab6c785209d87d4d2e7be544
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2019-12-28 11:40:42 +0700

    Refactor

--- apis/public/views.py
+++ apis/public/views.py
@@ -1,4 +1,6 @@
-from requests import post
+from typing import List
+
+from requests import post, Response as APIResponse
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -11,23 +13,28 @@ from externals.services import CreateReplyLineService
 class WebhookAPI(APIView):
     def post(self, request: Request) -> Response:
         events = request.data['events']
-
         is_verification = events[0]['replyToken'] == '00000000000000000000000000000000'
-        if is_verification:
-            return Response(None, HTTP_200_OK)
-
-        for event in events:
-            source_type = event['source']['type']
-            source_id = (
-                event['source']['groupId'] if source_type == 'group' else
-                event['source']['roomId'] if source_type == 'room' else
-                event['source']['userId']
-            )
-
-            url = reverse(f'webhook-{source_type}-api', [source_id], request=request)
-            post(url, event)
+        if not is_verification:
+            self.forward_events(request, events)
 
         return Response(None, HTTP_200_OK)
+
+    def forward_events(self, request: Request, events: list) -> List[APIResponse]:
+        return [
+            self.forward_event(request, event)
+            for event in events
+        ]
+
+    def forward_event(self, request: Request, event: dict) -> APIResponse:
+        source_type = event['source']['type']
+        source_id = (
+            event['source']['groupId'] if source_type == 'group' else
+            event['source']['roomId'] if source_type == 'room' else
+            event['source']['userId']
+        )
+        url = reverse(f'webhook-{source_type}-api', [source_id], request=request)
+
+        return post(url, event)
 
 
 class WebhookUserAPI(APIView):
