8c410fc0ea72196a584968fced51cd129ab4b536
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2020-01-12 14:56:30 +0700

    Remove models import

--- command/application.py
+++ command/application.py
@@ -1,7 +1,6 @@
 from chat import ChatApplication
 from dua_empat import DuaEmpatApplication
 from session import SessionApplication
-from session.models import Play
 
 
 class CommandApplication:
@@ -13,18 +12,18 @@ class CommandApplication:
 
         if command.startswith('main'):
             if command == 'main 24':
-                SessionApplication.start_game(source_id, Play.Game.DUA_EMPAT)
+                SessionApplication.start_game(source_id, 'dua_empat')
                 reply_texts = DuaEmpatApplication.start(source_id)
 
         elif command == 'udahan':
             game = SessionApplication.get_game(source_id)
-            if game is Play.Game.DUA_EMPAT:
+            if game == 'dua_empat':
                 SessionApplication.end_game(source_id)
                 reply_texts = DuaEmpatApplication.end(source_id)
 
         else:
             game = SessionApplication.get_game(source_id)
-            if game is Play.Game.DUA_EMPAT:
+            if game == 'dua_empat':
                 reply_texts = DuaEmpatApplication.reply(source_id, profile_id, text)
 
         ChatApplication.send_text_reply(token, reply_texts)
